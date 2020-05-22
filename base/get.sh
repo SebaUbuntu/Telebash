abc4f661c3f1875ba1f55c7dd8f35bd5cc718eeb
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-05-22 03:16:01 +0200

    get: add some comments for function arguments

--- base/get.sh
+++ base/get.sh
@@ -15,55 +15,66 @@
 # limitations under the License.
 #
 
-# Arguments: -d offset=$offset
+# Arguments: <curl arguments>
 get_updates() {
 	curl -s -X GET "$TG_API_URL/getUpdates" $@ | jq .
 }
 
+# Arguments: <curl arguments>
 get_unread_updates() {
 	get_updates $@ | jq ".result | .[]"
 }
 
+# Arguments: <curl arguments>
 get_unread_updates_number() {
 	get_updates $@ | jq ".result | length"
 }
 
+# Arguments: <curl arguments>
 get_last_update_id() {
 	UPDATE_NUMBER=$(get_unread_updates_number)
 	UPDATE_NUMBER=$(( "UPDATES_NUMBER" - "1" ))
 	get_updates $@ | jq ".result | .[$UPDATE_NUMBER] | .update_id"
 }
 
+# Arguments: <update number> <offset>
 get_specific_update() {
 	UPDATE_NUMBER=$1
 	shift
 	get_updates "offset=$2" | jq ".result | .[$UPDATE_NUMBER]"
 }
 
+# Arguments: <update in JSON>
 get_message() {
 	echo "$@" | jq ".message"
 }
 
+# Arguments: <message in JSON>
 get_reply_to_message() {
 	get_message "$@" | jq "{message: .reply_to_message}"
 }
 
+# Arguments: <message in JSON>
 get_message_id() {
 	get_message "$@" | jq ".message_id"
 }
 
+# Arguments: <message in JSON>
 get_sender_id() {
 	get_message "$@" | jq ".from.id"
 }
 
+# Arguments: <message in JSON>
 get_chat_id() {
 	get_message "$@" | jq ".chat.id"
 }
 
+# Arguments: <message in JSON>
 get_chat_type() {
 	get_message "$@" | jq ".chat.type"
 }
 
+# Arguments: <message in JSON>
 get_message_text() {
 	get_message "$@" | jq ".text" | cut -d "\"" -f 2
 }
