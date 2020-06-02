0cb74645978742a305f5a608edeaf5da7d3061db
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-06-02 02:50:59 +0200

    android_updates_module: set all variables to local

--- modules/android_updates/main.sh
+++ modules/android_updates/main.sh
@@ -17,24 +17,24 @@
 
 module_revengeos() {
 	if [ "$(tg_get_command_arguments "$@")" != "" ]; then
-		DEVICE_CODENAME="$(tg_get_command_arguments "$@")"
-		REVENGEOS_GENERIC_JSON="$(curl https://raw.githubusercontent.com/RevengeOS-Devices/official_devices/r10.0/maintainers.json | jq .)"
-		REVENGEOS_DEVICE_JSON="$(curl https://raw.githubusercontent.com/RevengeOS-Devices/official_devices/r10.0/$DEVICE_CODENAME/device.json | jq .)"
+		local DEVICE_CODENAME="$(tg_get_command_arguments "$@")"
+		local REVENGEOS_GENERIC_JSON="$(curl https://raw.githubusercontent.com/RevengeOS-Devices/official_devices/r10.0/maintainers.json | jq .)"
+		local REVENGEOS_DEVICE_JSON="$(curl https://raw.githubusercontent.com/RevengeOS-Devices/official_devices/r10.0/$DEVICE_CODENAME/device.json | jq .)"
 		if [ "$(echo "$REVENGEOS_GENERIC_JSON" | jq ".$DEVICE_CODENAME")" != "null" ]; then
-			REVENGEOS_DEVICE_INFO="RevengeOS Q build for $DEVICE_CODENAME
+			local REVENGEOS_DEVICE_INFO="RevengeOS Q build for $DEVICE_CODENAME
 
 Name: $(echo "$REVENGEOS_GENERIC_JSON" | jq ".$DEVICE_CODENAME.name" | cut -d "\"" -f 2)
 Mantainer: $(echo "$REVENGEOS_GENERIC_JSON" | jq ".$DEVICE_CODENAME.maintainer" | cut -d "\"" -f 2)
 Latest version: [$(echo "$REVENGEOS_DEVICE_JSON" | jq ".filename" | cut -d "\"" -f 2)]($(echo "$REVENGEOS_DEVICE_JSON" | jq ".url" | cut -d "\"" -f 2))"
 			if [ "$(echo "$REVENGEOS_GENERIC_JSON" | jq ".$DEVICE_CODENAME.xda_thread" | cut -d "\"" -f 2)" != "" ]; then
-				REVENGEOS_DEVICE_INFO="$REVENGEOS_DEVICE_INFO
+				local REVENGEOS_DEVICE_INFO="$REVENGEOS_DEVICE_INFO
 XDA thread: [Here]($(echo "$REVENGEOS_GENERIC_JSON" | jq ".$DEVICE_CODENAME.xda_thread" | cut -d "\"" -f 2))"
 			fi
 			if [ "$(echo "$REVENGEOS_DEVICE_JSON" | jq ".donate_url" | cut -d "\"" -f 2)" != "" ]; then
-				REVENGEOS_DEVICE_INFO="$REVENGEOS_DEVICE_INFO
+				local REVENGEOS_DEVICE_INFO="$REVENGEOS_DEVICE_INFO
 Donate: [Here]($(echo "$REVENGEOS_DEVICE_JSON" | jq ".donate_url" | cut -d "\"" -f 2))"
 			else
-				REVENGEOS_DEVICE_INFO="$REVENGEOS_DEVICE_INFO
+				local REVENGEOS_DEVICE_INFO="$REVENGEOS_DEVICE_INFO
 Donate: [Here](https://paypal.me/lucchetto)"
 			fi
 			tg_send_message "$(tg_get_chat_id "$@")" "$REVENGEOS_DEVICE_INFO" "$(tg_get_message_id "$@")"
