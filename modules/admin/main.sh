cf30cf82f240ccfe8e7033b98be380ba2513f7b7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-06-02 02:20:28 +0200

    admin_module: rewrite it
    - Add error checker
    - Make variables local
    - Save more variables to save time

--- modules/admin/main.sh
+++ modules/admin/main.sh
@@ -16,91 +16,180 @@
 #
 
 module_ban() {
-	if [ "$(tg_member_can_restrict_members "$(tg_get_chat_member "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$@")")")" = true ]; then
+	if [ "$(tg_member_can_restrict_members "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$@")")" = true ]; then
 		if [ "$(tg_get_sender_id "$(tg_get_reply_to_message "$@")")" != "null" ]; then
-			tg_ban_user "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$(tg_get_reply_to_message "$@")")"
-			tg_send_message "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$(tg_get_reply_to_message "$@")") banned successfully" "$(tg_get_message_id "$@")"
+			local USER_ID=$(tg_get_sender_id "$(tg_get_reply_to_message "$@")")
 		elif [ "$(tg_get_command_arguments "$@")" != "" ]; then
-			if echo "$(tg_get_command_arguments "$1")" | grep -q "@"; then
+			if echo "$(tg_get_command_arguments "$@")" | grep -q "@"; then
 				tg_send_message "$(tg_get_chat_id "$@")" "Banning through username is not supported, reply to a message or provide a user id" "$(tg_get_message_id "$@")"
 			else
-				tg_ban_user "$(tg_get_chat_id "$@")" "$(tg_get_command_arguments "$1")"
-				tg_send_message "$(tg_get_chat_id "$@")" "$(tg_get_command_arguments "$1") banned successfully" "$(tg_get_message_id "$@")"
+				local USER_ID=$(tg_get_command_arguments "$@")
 			fi
 		else
-			tg_send_message "$(tg_get_chat_id "$@")" "Please reply to a message or write the user id to unban that user" "$(tg_get_message_id "$@")"
+			tg_send_message "$(tg_get_chat_id "$@")" "Please reply to a message or write the user id to ban that user" "$(tg_get_message_id "$@")"
+		fi
+		if [ "$USER_ID" != "" ]; then
+			if [ "$USER_ID" = "$(tg_get_bot_user_id)" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "Why would I ban myself?" "$(tg_get_message_id "$@")"
+				local USER_ID=""
+			else
+				local RESULT=$(tg_ban_user "$(tg_get_chat_id "$@")" "$USER_ID")
+			fi
+		fi
+		if [ "$RESULT" != "" ]; then
+			if [ "$(echo $RESULT | jq .ok)" = "true" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "$USER_ID banned successfully" "$(tg_get_message_id "$@")"
+			elif [ "$(echo $RESULT | jq .description | cut -d "\"" -f 2)" = "Bad Request: user is an administrator of the chat" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "I can't ban this member, because it's an admin!" "$(tg_get_message_id "$@")"
+			elif [ "$(echo $RESULT | jq .description | cut -d "\"" -f 2)" = "Bad Request: USER_ID_INVALID" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "Invalid user ID!" "$(tg_get_message_id "$@")"
+			else
+				echo $RESULT | jq .description | cut -d "\"" -f 2
+				tg_send_message "$(tg_get_chat_id "$@")" "Unknown error!" "$(tg_get_message_id "$@")"
+			fi
 		fi
 	fi
 }
 
 module_unban() {
-	if [ "$(tg_member_can_restrict_members "$(tg_get_chat_member "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$@")")")" = true ]; then
+	if [ "$(tg_member_can_restrict_members "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$@")")" = true ]; then
 		if [ "$(tg_get_sender_id "$(tg_get_reply_to_message "$@")")" != "null" ]; then
-			tg_unban_user "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$(tg_get_reply_to_message "$@")")"
-			tg_send_message "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$(tg_get_reply_to_message "$@")") unbanned successfully" "$(tg_get_message_id "$@")"
+			local USER_ID=$(tg_get_sender_id "$(tg_get_reply_to_message "$@")")
 		elif [ "$(tg_get_command_arguments "$@")" != "" ]; then
-			if echo "$(tg_get_command_arguments "$1")" | grep -q "@"; then
+			if echo "$(tg_get_command_arguments "$@")" | grep -q "@"; then
 				tg_send_message "$(tg_get_chat_id "$@")" "Unbanning through username is not supported, reply to a message or provide a user id" "$(tg_get_message_id "$@")"
 			else
-				tg_unban_user "$(tg_get_chat_id "$@")" "$(tg_get_command_arguments "$1")"
-				tg_send_message "$(tg_get_chat_id "$@")" "$(tg_get_command_arguments "$1") kicked successfully" "$(tg_get_message_id "$@")"
+				local USER_ID=$(tg_get_command_arguments "$@")
 			fi
 		else
-			tg_send_message "$(tg_get_chat_id "$@")" "Please reply to a message or write the username to unban that user" "$(tg_get_message_id "$@")"
+			tg_send_message "$(tg_get_chat_id "$@")" "Please reply to a message or write the user id to unban that user" "$(tg_get_message_id "$@")"
+		fi
+		if [ "$USER_ID" != "" ]; then
+			if [ "$USER_ID" = "$(tg_get_bot_user_id)" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "Why would I unban myself?" "$(tg_get_message_id "$@")"
+				local USER_ID=""
+			else
+				local RESULT=$(tg_unban_user "$(tg_get_chat_id "$@")" "$USER_ID")
+			fi
+		fi
+		if [ "$RESULT" != "" ]; then
+			if [ "$(echo $RESULT | jq .ok)" = "true" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "$USER_ID unbanned successfully" "$(tg_get_message_id "$@")"
+			elif [ "$(echo $RESULT | jq .description | cut -d "\"" -f 2)" = "Bad Request: user is an administrator of the chat" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "I can't unban this member, because it's an admin (and it's already in the chat)!" "$(tg_get_message_id "$@")"
+			elif [ "$(echo $RESULT | jq .description | cut -d "\"" -f 2)" = "Bad Request: USER_ID_INVALID" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "Invalid user ID!" "$(tg_get_message_id "$@")"
+			else
+				tg_send_message "$(tg_get_chat_id "$@")" "Unknown error!" "$(tg_get_message_id "$@")"
+			fi
 		fi
 	fi
 }
 
 module_kick() {
-	if [ "$(tg_member_can_restrict_members "$(tg_get_chat_member "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$@")")")" = true ]; then
+	if [ "$(tg_member_can_restrict_members "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$@")")" = true ]; then
 		if [ "$(tg_get_sender_id "$(tg_get_reply_to_message "$@")")" != "null" ]; then
-			tg_kick_user "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$(tg_get_reply_to_message "$@")")"
-			tg_send_message "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$(tg_get_reply_to_message "$@")") kicked successfully" "$(tg_get_message_id "$@")"
+			local USER_ID=$(tg_get_sender_id "$(tg_get_reply_to_message "$@")")
 		elif [ "$(tg_get_command_arguments "$@")" != "" ]; then
-			if echo "$(tg_get_command_arguments "$1")" | grep -q "@"; then
+			if echo "$(tg_get_command_arguments "$@")" | grep -q "@"; then
 				tg_send_message "$(tg_get_chat_id "$@")" "Kicking through username is not supported, reply to a message or provide a user id" "$(tg_get_message_id "$@")"
 			else
-				tg_kick_user "$(tg_get_chat_id "$@")" "$(tg_get_command_arguments "$1")"
-				tg_send_message "$(tg_get_chat_id "$@")" "$(tg_get_command_arguments "$1") kicked successfully" "$(tg_get_message_id "$@")"
+				local USER_ID=$(tg_get_command_arguments "$@")
 			fi
 		else
-			tg_send_message "$(tg_get_chat_id "$@")" "Please reply to a message or write the username to kick that user" "$(tg_get_message_id "$@")"
+			tg_send_message "$(tg_get_chat_id "$@")" "Please reply to a message or write the user id to kick that user" "$(tg_get_message_id "$@")"
+		fi
+		if [ "$USER_ID" != "" ]; then
+			if [ "$USER_ID" = "$(tg_get_bot_user_id)" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "Why would I kick myself?" "$(tg_get_message_id "$@")"
+				local USER_ID=""
+			else
+				local RESULT=$(tg_kick_user "$(tg_get_chat_id "$@")" "$USER_ID")
+			fi
+		fi
+		if [ "$RESULT" != "" ]; then
+			if [ "$(echo $RESULT | jq .ok)" = "true" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "$USER_ID kicked successfully" "$(tg_get_message_id "$@")"
+			elif [ "$(echo $RESULT | jq .description | cut -d "\"" -f 2)" = "Bad Request: user is an administrator of the chat" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "I can't kick this member, because it's an admin!" "$(tg_get_message_id "$@")"
+			elif [ "$(echo $RESULT | jq .description | cut -d "\"" -f 2)" = "Bad Request: USER_ID_INVALID" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "Invalid user ID!" "$(tg_get_message_id "$@")"
+			else
+				tg_send_message "$(tg_get_chat_id "$@")" "Unknown error!" "$(tg_get_message_id "$@")"
+			fi
 		fi
 	fi
 }
 
 module_mute() {
-	if [ "$(tg_member_can_restrict_members "$(tg_get_chat_member "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$@")")")" = true ]; then
+	if [ "$(tg_member_can_restrict_members "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$@")")" = true ]; then
 		if [ "$(tg_get_sender_id "$(tg_get_reply_to_message "$@")")" != "null" ]; then
-			tg_mute_user "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$(tg_get_reply_to_message "$@")")"
-			tg_send_message "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$(tg_get_reply_to_message "$@")") muted successfully" "$(tg_get_message_id "$@")"
+			local USER_ID=$(tg_get_sender_id "$(tg_get_reply_to_message "$@")")
 		elif [ "$(tg_get_command_arguments "$@")" != "" ]; then
-			if echo "$(tg_get_command_arguments "$1")" | grep -q "@"; then
+			if echo "$(tg_get_command_arguments "$@")" | grep -q "@"; then
 				tg_send_message "$(tg_get_chat_id "$@")" "Muting through username is not supported, reply to a message or provide a user id" "$(tg_get_message_id "$@")"
 			else
-				tg_mute_user "$(tg_get_chat_id "$@")" "$(tg_get_command_arguments "$1")"
-				tg_send_message "$(tg_get_chat_id "$@")" "$(tg_get_command_arguments "$1") muted successfully" "$(tg_get_message_id "$@")"
+				local USER_ID=$(tg_get_command_arguments "$@")
 			fi
 		else
-			tg_send_message "$(tg_get_chat_id "$@")" "Please reply to a message or write the username to mute that user" "$(tg_get_message_id "$@")"
+			tg_send_message "$(tg_get_chat_id "$@")" "Please reply to a message or write the user id to mute that user" "$(tg_get_message_id "$@")"
+		fi
+		if [ "$USER_ID" != "" ]; then
+			if [ "$USER_ID" = "$(tg_get_bot_user_id)" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "Why would I mute myself?" "$(tg_get_message_id "$@")"
+				local USER_ID=""
+			else
+				local RESULT=$(tg_mute_user "$(tg_get_chat_id "$@")" "$USER_ID")
+			fi
+		fi
+		if [ "$RESULT" != "" ]; then
+			if [ "$(echo $RESULT | jq .ok)" = "true" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "$USER_ID muted successfully" "$(tg_get_message_id "$@")"
+			elif [ "$(echo $RESULT | jq .description | cut -d "\"" -f 2)" = "Bad Request: user is an administrator of the chat" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "I can't mute this member, because it's an admin!" "$(tg_get_message_id "$@")"
+			elif [ "$(echo $RESULT | jq .description | cut -d "\"" -f 2)" = "Bad Request: USER_ID_INVALID" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "Invalid user ID!" "$(tg_get_message_id "$@")"
+			else
+				tg_send_message "$(tg_get_chat_id "$@")" "Unknown error!" "$(tg_get_message_id "$@")"
+			fi
 		fi
 	fi
 }
 
 module_unmute() {
-	if [ "$(tg_member_can_restrict_members "$(tg_get_chat_member "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$@")")")" = true ]; then
+	if [ "$(tg_member_can_restrict_members "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$@")")" = true ]; then
 		if [ "$(tg_get_sender_id "$(tg_get_reply_to_message "$@")")" != "null" ]; then
-			tg_unmute_user "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$(tg_get_reply_to_message "$@")")"
-			tg_send_message "$(tg_get_chat_id "$@")" "$(tg_get_sender_id "$(tg_get_reply_to_message "$@")") unmuted successfully" "$(tg_get_message_id "$@")"
+			local USER_ID=$(tg_get_sender_id "$(tg_get_reply_to_message "$@")")
 		elif [ "$(tg_get_command_arguments "$@")" != "" ]; then
-			if echo "$(tg_get_command_arguments "$1")" | grep -q "@"; then
+			if echo "$(tg_get_command_arguments "$@")" | grep -q "@"; then
 				tg_send_message "$(tg_get_chat_id "$@")" "Unmuting through username is not supported, reply to a message or provide a user id" "$(tg_get_message_id "$@")"
 			else
-				tg_unmute_user "$(tg_get_chat_id "$@")" "$(tg_get_command_arguments "$1")"
-				tg_send_message "$(tg_get_chat_id "$@")" "$(tg_get_command_arguments "$1") unmuted successfully" "$(tg_get_message_id "$@")"
+				local USER_ID=$(tg_get_command_arguments "$@")
 			fi
 		else
-			tg_send_message "$(tg_get_chat_id "$@")" "Please reply to a message or write the username to unmute that user" "$(tg_get_message_id "$@")"
+			tg_send_message "$(tg_get_chat_id "$@")" "Please reply to a message or write the user id to unmute that user" "$(tg_get_message_id "$@")"
+		fi
+		if [ "$USER_ID" != "" ]; then
+			if [ "$USER_ID" = "$(tg_get_bot_user_id)" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "Why would I unmute myself?" "$(tg_get_message_id "$@")"
+				local USER_ID=""
+			else
+				local RESULT=$(tg_unmute_user "$(tg_get_chat_id "$@")" "$USER_ID")
+			fi
+		fi
+		if [ "$RESULT" != "" ]; then
+			if [ "$(echo $RESULT | jq .ok)" = "true" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "$USER_ID unmuted successfully" "$(tg_get_message_id "$@")"
+			elif [ "$(echo $RESULT | jq .description | cut -d "\"" -f 2)" = "Bad Request: user is an administrator of the chat" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "I can't unmute this member, because it's an admin!" "$(tg_get_message_id "$@")"
+			elif [ "$(echo $RESULT | jq .description | cut -d "\"" -f 2)" = "Bad Request: USER_ID_INVALID" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "Invalid user ID!" "$(tg_get_message_id "$@")"
+			elif [ "$(echo $RESULT | jq .description | cut -d "\"" -f 2)" = "Bad Request: CHAT_ADMIN_REQUIRED" ]; then
+				tg_send_message "$(tg_get_chat_id "$@")" "I can't unmute this member, because I don't have the permission to do it, or the member is an admin!" "$(tg_get_message_id "$@")"
+			else
+				echo $RESULT | jq .description | cut -d "\"" -f 2
+				tg_send_message "$(tg_get_chat_id "$@")" "Unknown error!" "$(tg_get_message_id "$@")"
+			fi
 		fi
 	fi
 }
