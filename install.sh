51e4136b375c6b97b934e6d4fabb2f80eada58ca
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-06-27 00:41:09 +0200

    bot: add a dependencies installer
    * This will install all the necessary dependencies, execute it before starting the bot for the first time

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Copyright (C) 2020 SebaUbuntu's HomeBot
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Install dependencies
+# Note: if you want to add a package manager, PR it
+DEPENDENCIES="bash jq cowsay neofetch"
+if [ "$(command -v apt-get)" != "" ]; then
+	sudo apt-get install $DEPENDENCIES
+elif [ "$(command -v pacman)" != "" ]; then
+	sudo pacman -S $DEPENDENCIES
+else
+	echo "Distro not supported, please install dependencies by yourself"
+fi
+
+# Install labbots/google-drive-upload (be sure to configure it before enabling it with CI variable)
+bash <(curl --compressed -s https://raw.githubusercontent.com/labbots/google-drive-upload/master/install.sh)
+echo "gupload function has been installed, but be sure to configure it before enabling it with CI variable"
+echo "For informations, see this https://github.com/labbots/google-drive-upload#generating-oauth-credentials"
