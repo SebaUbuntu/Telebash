badc2697671503672f6667e949b7b46f22a8b4a9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-07-04 12:32:56 +0200

    telegram_send: Fix media sending

--- base/telegram_send.sh
+++ base/telegram_send.sh
@@ -39,45 +39,45 @@ tg_send_message() {
 # Arguments: <chatid> <file_path> <message_id (for reply, optional)>
 tg_send_photo() {
 	if [ "$3" != "" ]; then
-		curl -s -S -X POST "$TG_API_URL/sendPhoto" -d chat_id="$1" -d reply_to_message_id="$3" -F photo=@"$2" -H "Content-Type:multipart/form-data" | jq .
+		curl -s -S "$TG_API_URL/sendPhoto?chat_id=${1}&reply_to_message_id=${3}" -F photo=@"$2" | jq .
 	else
-		curl -s -S -X POST "$TG_API_URL/sendPhoto" -d chat_id="$1" -F photo=@"$2" -H "Content-Type:multipart/form-data" | jq .
+		curl -s -S "$TG_API_URL/sendPhoto?chat_id=${1}" -F photo=@"$2" | jq .
 	fi
 }
 
 # Arguments: <chatid> <file_path> <message_id (for reply, optional)>
 tg_send_audio() {
 	if [ "$3" != "" ]; then
-		curl -s -S -X POST "$TG_API_URL/sendAudio" -d chat_id="$1" -d reply_to_message_id="$3" -F audio=@"$2" -H "Content-Type:multipart/form-data" | jq .
+		curl -s -S "$TG_API_URL/sendAudio?chat_id=${1}&reply_to_message_id=${3}" -F audio=@"$2" | jq .
 	else
-		curl -s -S -X POST "$TG_API_URL/sendAudio" -d chat_id="$1" -F audio=@"$2" -H "Content-Type:multipart/form-data" | jq .
+		curl -s -S "$TG_API_URL/sendAudio?chat_id=${1}" -F audio=@"$2" | jq .
 	fi
 }
 
 # Arguments: <chatid> <file_path> <message_id (for reply, optional)>
 tg_send_document() {
 	if [ "$3" != "" ]; then
-		curl -s -S -X POST "$TG_API_URL/sendDocument" -d chat_id="$1" -d reply_to_message_id="$3" -F document=@"$2" -H "Content-Type:multipart/form-data" | jq .
+		curl -s -S "$TG_API_URL/sendDocument?chat_id=${1}&reply_to_message_id=${3}" -F document=@"$2" | jq .
 	else
-		curl -s -S -X POST "$TG_API_URL/sendDocument" -d chat_id="$1" -F document=@"$2" -H "Content-Type:multipart/form-data" | jq .
+		curl -s -S "$TG_API_URL/sendDocument?chat_id=${1}" -F document=@"$2" | jq .
 	fi
 }
 
 # Arguments: <chatid> <file_path> <message_id (for reply, optional)>
 tg_send_video() {
 	if [ "$3" != "" ]; then
-		curl -s -S -X POST "$TG_API_URL/sendVideo" -d chat_id="$1" -d reply_to_message_id="$3" -F video=@"$2" -H "Content-Type:multipart/form-data" | jq .
+		curl -s -S "$TG_API_URL/sendVideo?chat_id=${1}&reply_to_message_id=${3}" -F video=@"$2" | jq .
 	else
-		curl -s -S -X POST "$TG_API_URL/sendVideo" -d chat_id="$1" -F video=@"$2" -H "Content-Type:multipart/form-data" | jq .
+		curl -s -S "$TG_API_URL/sendVideo?chat_id=${1}" -F video=@"$2" | jq .
 	fi
 }
 
 # Arguments: <chatid> <file_path> <message_id (for reply, optional)>
 tg_send_animation() {
 	if [ "$3" != "" ]; then
-		curl -s -S -X POST "$TG_API_URL/sendAnimation" -d chat_id="$1" -d reply_to_message_id="$3" -F animation=@"$2" -H "Content-Type:multipart/form-data" | jq .
+		curl -s -S "$TG_API_URL/sendAnimation?chat_id=${1}&reply_to_message_id=${3}" -F animation=@"$2" | jq .
 	else
-		curl -s -S -X POST "$TG_API_URL/sendAnimation" -d chat_id="$1" -F animation=@"$2" -H "Content-Type:multipart/form-data" | jq .
+		curl -s -S "$TG_API_URL/sendAnimation?chat_id=${1}" -F animation=@"$2" | jq .
 	fi
 }
 
