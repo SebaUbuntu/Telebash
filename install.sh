ccb7cad91038dfca457d4beb435c4e710c3cb511
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-07-06 15:54:00 +0200

    install: Add speedtest-cli
    * And also improve it a little

--- install.sh
+++ install.sh
@@ -17,16 +17,25 @@
 
 # Install dependencies
 # Note: if you want to add a package manager, PR it
-DEPENDENCIES="bash jq cowsay neofetch"
+COMMON_DEPENDENCIES="bash jq cowsay neofetch"
+APT_DEPENDENCIES="speedtest-cli"
+PACMAN_DEPENDENCIES=""
 if [ "$(command -v apt-get)" != "" ]; then
-	sudo apt-get install $DEPENDENCIES
+	sudo apt-get install $COMMON_DEPENDENCIES $APT_DEPENDENCIES
+	if [ "$PACMAN_DEPENDENCIES" != "" ]; then
+		echo "Note: You must install for yourself the following packages: $PACMAN_DEPENDENCIES"
+	fi
 elif [ "$(command -v pacman)" != "" ]; then
-	sudo pacman -S $DEPENDENCIES
+	sudo pacman -S $COMMON_DEPENDENCIES $PACMAN_DEPENDENCIES
+	if [ "$APT_DEPENDENCIES" != "" ]; then
+		echo "Note: You must install for yourself the following packages: $APT_DEPENDENCIES"
+	fi
 else
-	echo "Distro not supported, please install dependencies by yourself"
+	echo "Distro not supported, please install the following dependencies by yourself: $COMMON_DEPENDENCIES $APT_DEPENDENCIES $PACMAN_DEPENDENCIES"
 fi
 
 # Install labbots/google-drive-upload (be sure to configure it before enabling it with CI variable)
-bash <(curl --compressed -s https://raw.githubusercontent.com/labbots/google-drive-upload/master/install.sh)
+bash <(curl --compressed -s https://raw.githubusercontent.com/labbots/google-drive-upload/master/install.sh) > /dev/null 2>&1
 echo "gupload function has been installed, but be sure to configure it before enabling it with CI variable"
 echo "For informations, see this https://github.com/labbots/google-drive-upload#generating-oauth-credentials"
+echo "All done!"
