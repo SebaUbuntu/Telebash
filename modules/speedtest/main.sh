fd96391569d8d72706dc3fcbd654c303fc7107fd
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-07-06 15:08:02 +0200

    speedtest_module: Initial release

---
+++ modules/speedtest/main.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (C) 2020 SebaUbuntu's HomeBot
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module_speedtest() {
+	local MESSAGE_ID=$(tg_send_message "$(tg_get_chat_id "$@")" "Running speedtest..." "$(tg_get_message_id "$@")" | jq .result.message_id)
+	tg_edit_message_text "$(tg_get_chat_id "$@")" "$MESSAGE_ID" "\`$(speedtest-cli | grep "Mbit/s")\`" --markdown
+}
