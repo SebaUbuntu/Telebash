3265ac194659e8a32514e08bcb7e15bc01014ef9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-09-01 12:54:01 +0200

    speedtest_module: Adapt to new changes

--- modules/speedtest/main.sh
+++ modules/speedtest/main.sh
@@ -16,6 +16,6 @@
 #
 
 module_speedtest() {
-	local MESSAGE_ID=$(tg_send_message --chat_id "$(tg_get_chat_id "$@")" --text "Running speedtest..." --reply_to_message_id "$(tg_get_message_id "$@")" | jq .result.message_id)
-	tg_edit_message_text --chat_id "$(tg_get_chat_id "$@")" --message_id "$MESSAGE_ID" --text "\`$(speedtest-cli | grep "Mbit/s")\`" --parse_mode "Markdown"
+	local MESSAGE_ID=$(telegram sendMessage --chat_id "$(tg_get_chat_id "$@")" --text "Running speedtest..." --reply_to_message_id "$(tg_get_message_id "$@")" | jq .result.message_id)
+	telegram editMessageText --chat_id "$(tg_get_chat_id "$@")" --message_id "$MESSAGE_ID" --text "\`$(speedtest-cli | grep "Mbit/s")\`" --parse_mode "Markdown"
 }
