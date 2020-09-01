7726612d719ce84da84653825475a7a6353eafa3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-09-01 11:41:19 +0200

    telegram: Move main functions to telegram.sh

--- base/telegram.sh
+++ base/telegram.sh
@@ -17,7 +17,6 @@
 
 # Main wrapper, source all the components
 source variables.sh
-source base/telegram_base.sh
 source base/telegram_admin.sh
 source base/telegram_get.sh
 source base/telegram_send.sh
@@ -25,3 +24,57 @@ source base/admin.sh
 source base/get.sh
 source base/modules.sh
 source base/updates.sh
+
+telegram_main() {
+	local ACTION=${1}
+	local HTTP_REQUEST=${2}
+	local CURL_ARGUMENTS=()
+	while [ "${#}" -gt 0 ]; do
+		case "${1}" in
+			--animation | --audio | --document | --photo | --video )
+				local CURL_ARGUMENTS+=(-F $(echo "${1}" | sed 's/--//')=@"${2}")
+				shift
+				;;
+			--* )
+				if [ "$HTTP_REQUEST" != "POST_FILE" ]; then
+					local CURL_ARGUMENTS+=(-d $(echo "${1}" | sed 's/--//')="${2}")
+				else
+					local CURL_ARGUMENTS+=(-F $(echo "${1}" | sed 's/--//')="${2}")
+				fi
+				shift
+				;;
+		esac
+		shift
+	done
+	telegram_curl "$ACTION" "$HTTP_REQUEST" "${CURL_ARGUMENTS[@]}"
+}
+
+telegram_curl() {
+	local ACTION=${1}
+	shift
+	local HTTP_REQUEST=${1}
+	shift
+	if [ "$HTTP_REQUEST" != "POST_FILE" ]; then
+		curl -s -X $HTTP_REQUEST "https://api.telegram.org/bot$TG_BOT_TOKEN/$ACTION" "$@" | jq .
+	else
+		curl -s "https://api.telegram.org/bot$TG_BOT_TOKEN/$ACTION" "$@" | jq .
+	fi
+}
+
+telegram_curl_get() {
+	local ACTION=${1}
+	shift
+	telegram_main "$ACTION" GET "$@"
+}
+
+telegram_curl_post() {
+	local ACTION=${1}
+	shift
+	telegram_main "$ACTION" POST "$@"
+}
+
+telegram_curl_post_file() {
+	local ACTION=${1}
+	shift
+	telegram_main "$ACTION" POST_FILE "$@"
+}
