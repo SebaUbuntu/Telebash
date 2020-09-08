260b55014ee59a9521445e987098387223b1b15c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-09-08 13:07:43 +0200

    ci_module: Fix derp

--- modules/ci/main.sh
+++ modules/ci/main.sh
@@ -25,9 +25,9 @@ module_ci() {
 		if [ "$CI_CHANNEL_ID" != "" ]; then
 			modules/ci/build.sh "$@" &
 		else
-			telegram sendMessage --chat_id "$(tg_get_chat_id "$@")" --text "Error: CI channel or user ID not defined" --reply_to_message_text "$(tg_get_message_id "$@")"
+			telegram sendMessage --chat_id "$(tg_get_chat_id "$@")" --text "Error: CI channel or user ID not defined" --reply_to_message_id "$(tg_get_message_id "$@")"
 		fi
 	else
-		telegram sendMessage --chat_id "$(tg_get_chat_id "$@")" --text "Error: you are not authorized to use CI function of this bot, ask to who host this bot to add you to the authorized people list" --reply_to_message_text "$(tg_get_message_id "$@")"
+		telegram sendMessage --chat_id "$(tg_get_chat_id "$@")" --text "Error: you are not authorized to use CI function of this bot, ask to who host this bot to add you to the authorized people list" --reply_to_message_id "$(tg_get_message_id "$@")"
 	fi
 }
