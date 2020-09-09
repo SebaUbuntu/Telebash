d67335958125e1e1aa0a2cafea7e9008d18315bf
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-09-09 12:31:31 +0200

    ci_module: FluidOS -> Fluid

--- modules/ci/ProjectFluid-10.0.sh
+++ modules/ci/ProjectFluid-10.0.sh
@@ -27,7 +27,7 @@ CI_LUNCH_SUFFIX=userdebug
 # Target to build (e.g. to build a ROM's OTA package, use "bacon" or "otapackage", for a recovery project, use "recoveryimage")
 CI_BUILD_TARGET=bacon
 # Filename of the output. You can also use wildcards if the name isn't fixed
-CI_OUT_ARTIFACTS_NAME=FluidOS-*.zip
+CI_OUT_ARTIFACTS_NAME=Fluid-*.zip
 
 # Don't touch this line, unless you know what you are doing
 modules/ci/AOSP.sh "$@" --project "$CI_AOSP_PROJECT" --name "$CI_AOSP_PROJECT_NAME" --version "$CI_AOSP_PROJECT_VERSION" --lunch_prefix "$CI_LUNCH_PREFIX" --lunch_suffix "$CI_LUNCH_SUFFIX" --build_target "$CI_BUILD_TARGET" --artifacts "$CI_OUT_ARTIFACTS_NAME"
