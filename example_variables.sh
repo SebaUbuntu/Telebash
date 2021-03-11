c25e07ea830f7f07cc7ca430237d0f32290797f7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-11 15:47:47 +0100

    modules: Kill modules that require additional packages
    * I'm not using this as a bot anymore, I'm more interested on using this as a wrapper, so just leave modules that demonstrate how it works

--- example_variables.sh
+++ example_variables.sh
@@ -9,20 +9,6 @@
 # Add here your HTTP API bot token, get it from @BotFather
 export TG_BOT_TOKEN=
 
-# Module - CI
-# Read this before continuing: https://github.com/SebaUbuntu/Telebash/wiki/Module-%7C-CI#variables
-export CI_APPROVED_USER_IDS=
-export CI_MAIN_DIR=
-export CI_CHANNEL_ID=
-export CI_UPLOAD_ARTIFACTS=false
-export CI_ARTIFACTS_UPLOAD_METHOD=
-# If you use SourceForge upload method, please fill the following variables:
-export CI_SF_PROJECT=
-export CI_SF_USER=
-export CI_SF_PASS=
-# If you use local copy method, please fill the following variables:
-export CI_LOCAL_COPY_DIR=
-
 # Module - Weather
 # Read this before continuing: https://github.com/SebaUbuntu/Telebash/wiki/Module-%7C-Weather#variables
 export WEATHER_API_KEY=
--- install.sh
+++ install.sh
@@ -7,30 +7,13 @@
 
 # Install dependencies
 # Note: if you want to add a package manager, PR it
-COMMON_DEPENDENCIES="bash jq cowsay neofetch openssh sshpass"
-APT_DEPENDENCIES="speedtest-cli"
-PACMAN_DEPENDENCIES=""
+DEPENDENCIES="bash jq"
 if [ "$(command -v apt-get)" != "" ]; then
-	sudo apt-get install ${COMMON_DEPENDENCIES} ${APT_DEPENDENCIES}
-	if [ "${PACMAN_DEPENDENCIES}" != "" ]; then
-		echo "Note: You must install for yourself the following packages: ${PACMAN_DEPENDENCIES}"
-	fi
+	sudo apt-get install ${DEPENDENCIES}
 elif [ "$(command -v pacman)" != "" ]; then
-	sudo pacman -S ${COMMON_DEPENDENCIES} ${PACMAN_DEPENDENCIES}
-	if [ "${APT_DEPENDENCIES}" != "" ]; then
-		echo "Note: You must install for yourself the following packages: ${APT_DEPENDENCIES}"
-	fi
+	sudo pacman -S ${DEPENDENCIES}
 else
-	echo "Distro not supported, please install the following dependencies by yourself: ${COMMON_DEPENDENCIES} ${APT_DEPENDENCIES} ${PACMAN_DEPENDENCIES}"
+	echo "Distro not supported, please install the following dependencies by yourself: ${DEPENDENCIES}"
 fi
-
-# Install labbots/google-drive-upload (be sure to configure it before enabling it with CI variable)
-bash <(curl --compressed -s "https://raw.githubusercontent.com/labbots/google-drive-upload/master/install.sh") &> /dev/null
-echo "gupload function has been installed, but be sure to configure it before enabling it with CI variable"
-echo "For informations, see this https://github.com/labbots/google-drive-upload#generating-oauth-credentials"
-
-# Add SourceForge server to the list of known hosts
-[ ! -d ~/.ssh ] && mkdir ~/.ssh
-ssh-keyscan frs.sourceforge.net >> ~/.ssh/known_hosts
 
 echo "All done!"
